2057455ef46dd63616af6c52755284a351dcd6cb
ivankrtv <mega.krotov@mail.ru>
2023-08-12 18:27:23 +0300

    fix: add index file

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { OpenApiDoc } from './lib/OpenApiDoc';
+import {
+  StringProperty,
+  IntProperty,
+  FloatProperty,
+  ArrayProperty,
+  BoolProperty,
+  EnumProperty,
+  ObjectProperty,
+} from './lib/decorators/ApiProperty.decorators';
+
+export {
+  OpenApiDoc,
+  StringProperty,
+  IntProperty,
+  ArrayProperty,
+  FloatProperty,
+  BoolProperty,
+  EnumProperty,
+  ObjectProperty,
+};
